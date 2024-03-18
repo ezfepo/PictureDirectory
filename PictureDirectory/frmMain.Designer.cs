16953f7669bec6f84082fec46791dad3a8729e5c
Ezequiel Fernández Ponce <20102608+ezfepo@users.noreply.github.com>
2024-03-18 12:39:56 -0300

    base code

---
+++ PictureDirectory/frmMain.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PictureDirectory
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.fbdMain = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(760, 12);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 31);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // fbdMain
+            // 
+            this.fbdMain.ShowNewFolderButton = false;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(12, 12);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 31);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDirectory.Location = new System.Drawing.Point(12, 49);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(823, 22);
+            this.txtDirectory.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 81);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnProcess);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Picture Directory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.FolderBrowserDialog fbdMain;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtDirectory;
+    }
+}
+
---
+++ PictureDirectory/frmMain.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PictureDirectory
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbdMain.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtDirectory.Text = fbdMain.SelectedPath;
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            string[] oFiles = Directory.GetFiles(txtDirectory.Text);
+
+            foreach (string oFile in oFiles)
+            {
+                try
+                {
+                    string oFileName = System.IO.Path.GetFileName(oFile);
+                    string oExtension = System.IO.Path.GetExtension(oFile).ToLower();
+
+                    switch (oExtension)
+                    {
+                        case ".3gp":
+                        case ".mp4":
+                        case ".mov":
+                        case ".png":
+                        case ".jpg":
+                        case ".jpeg":
+                            //FileAttributes oFileAttributes = File.GetAttributes(oFile);
+
+                            DateTime oMinDateTime = DateTime.Today;
+
+                            oMinDateTime = new DateTime(Math.Min(File.GetCreationTime(oFile).Ticks, oMinDateTime.Ticks));
+                            oMinDateTime = new DateTime(Math.Min(File.GetLastAccessTime(oFile).Ticks, oMinDateTime.Ticks));
+                            oMinDateTime = new DateTime(Math.Min(File.GetLastWriteTime(oFile).Ticks, oMinDateTime.Ticks));
+
+                            IReadOnlyList<MetadataExtractor.Directory> oDirectories = MetadataExtractor.ImageMetadataReader.ReadMetadata(oFile);
+                            MetadataExtractor.Formats.Exif.ExifSubIfdDirectory oExifSubIfdDirectory = oDirectories.OfType<MetadataExtractor.Formats.Exif.ExifSubIfdDirectory>().FirstOrDefault();
+                            if (oExifSubIfdDirectory != null)
+                            {
+                                object oDateTime = oExifSubIfdDirectory.GetObject(MetadataExtractor.Formats.Exif.ExifDirectoryBase.TagDateTimeOriginal);
+                                if (oDateTime != null)
+                                {
+                                    DateTime oTagDateTimeOriginal = DateTime.ParseExact(oDateTime.ToString().Substring(0, 10), "yyyy:MM:dd", System.Threading.Thread.CurrentThread.CurrentCulture);
+                                    oMinDateTime = new DateTime(Math.Min(oTagDateTimeOriginal.Ticks, oMinDateTime.Ticks));
+                                }
+                            }
+
+                            if (oFileName.StartsWith("IMG-") || oFileName.StartsWith("VID_") || oFileName.StartsWith("VID-"))
+                            {
+                                DateTime oDateTimeWhatsapp = DateTime.Today;
+                                if (DateTime.TryParseExact(oFileName.Substring(4, 8), "yyyyMMdd", System.Threading.Thread.CurrentThread.CurrentCulture, System.Globalization.DateTimeStyles.None, out oDateTimeWhatsapp))
+                                {
+                                    oMinDateTime = new DateTime(Math.Min(oDateTimeWhatsapp.Ticks, oMinDateTime.Ticks));
+                                }
+                            }
+
+                            //Console.WriteLine(oFile + " " + oCreationTime.ToShortDateString() + " " + oLastAccessTime.ToShortDateString() + " " + oLastWriteTime.ToShortDateString());
+
+                            string oDirectory = oMinDateTime.ToString("yyyy-MM-dd");
+
+                            oDirectory = Path.Combine(txtDirectory.Text, oDirectory);
+                            if (!Directory.Exists(oDirectory)) { Directory.CreateDirectory(oDirectory); }
+
+                            Console.WriteLine(oFile + " " + oDirectory);
+
+                            string newFile = Path.Combine(oDirectory, oFileName);
+                            if (!File.Exists(newFile)) { File.Move(oFile, newFile); }
+
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    //throw;
+                }
+            }
+        }
+    }
+}
